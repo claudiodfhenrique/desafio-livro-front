b8e264b39db13562226f904c23954b7c76471d7a
Cláudio Henrique <claudiodf.henrique@gmail.com>
2024-10-26 16:23:53 -0300

    feat: CRUD livro

--- src/app/pages/livros/livros.component.ts
+++ src/app/pages/livros/livros.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnInit, signal, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -11,6 +11,11 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { LivroService } from '../../shared/services/livro.service';
 import { lastValueFrom } from 'rxjs';
+import { Livro } from '../../shared/models/livro.model';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginatorIntlonf } from '../../shared/injectors/mat-paginator-intl-conf.injector';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-livros',
@@ -27,22 +32,37 @@ import { lastValueFrom } from 'rxjs';
     MatSnackBarModule,
     HttpClientModule,
     MatProgressSpinnerModule,
+    MatPaginator,
+    MatPaginatorModule,    
+    MatTableModule,
+    MatIconModule
   ],
   providers: [
-    LivroService
+    LivroService,
+    { provide: MatPaginatorIntl, useClass: MatPaginatorIntlonf }
   ],
   templateUrl: './livros.component.html',
   styleUrl: './livros.component.css'
 })
-export class LivrosComponent implements OnInit {
+export class LivrosComponent implements OnInit { 
   public isLoading = signal(false);
+  
+  public isLoadingTable = signal(true);
+
   public formGroup: FormGroup;
+
+  public displayedColumns: string[] = ['cod', 'titulo', 'editora', 'edicao', 'anoPublicacao', 'acoes'];
+
+  public dataSource!: MatTableDataSource<Livro>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(
     private fb: FormBuilder,
     private service: LivroService,
     private snackBar: MatSnackBar) {
     this.formGroup = this.fb.group({
+      cod: [],
       titulo: [null, [Validators.required, Validators.maxLength(100)]],
       editora: [null, [Validators.required, Validators.maxLength(100)]],
       edicao: [null, Validators.required],
@@ -50,17 +70,53 @@ export class LivrosComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {    
+  async ngOnInit(): Promise<void> {    
+    await this.loadData();
+  }
+
+  private async loadData(): Promise<void> {
+    const data = await lastValueFrom(this.service.all());
+    this.dataSource = new MatTableDataSource<Livro>(data);    
+    this.dataSource!.paginator = this.paginator;    
+    this.isLoadingTable.set(false);
+  }
+
+  public async delete(codAu: number): Promise<void> {    
+    await lastValueFrom(this.service.delete(codAu));    
+    this.snackBar.open('Livro excluído com sucesso!', 'fechar');
+    await this.loadData();    
+    this.formGroup.markAsPristine();
+  }
+
+  public edit(autor: Livro) : void {
+    this.formGroup.patchValue(autor);
+  }
+
+  private async create(autor: Livro): Promise<void> {
+    await lastValueFrom(this.service.create(autor));    
+    this.snackBar.open('Livro cadastrado com sucesso!', 'fechar');
+  }
+
+  private async update(autor: Livro): Promise<void> {
+    await lastValueFrom(this.service.put(autor));    
+    this.snackBar.open('Livro atualizado com sucesso!', 'fechar');
   }
 
   async save(): Promise<void> {
     if (this.formGroup.valid) {
+      const model = this.formGroup.value as Livro;
       this.isLoading.set(true);
-      await lastValueFrom(this.service.create(this.formGroup.value));
+
+      if (model.cod) {
+        await this.update(model);
+      }
+      else {
+        await this.create(model);
+      }
+      
       this.isLoading.set(false);
-      this.snackBar.open('Autor cadastrado com sucesso!');
-      this.formGroup.markAsPristine();
       this.formGroup.reset();
+      await this.loadData();
     }
   }
 }
